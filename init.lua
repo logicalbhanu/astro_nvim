b024f2f4508ec9d677af29f59cce1b7c5e5a39d0
Ubuntu <ubuntu@instance-4.us-central1-a.c.crack-walker-265406.internal>
2023-08-16 04:10:04 +0000

    add default python for neovim

--- init.lua
+++ init.lua
@@ -17,6 +17,7 @@ return {
     },
   },
 
+
   -- Set colorscheme to use
   colorscheme = "astrodark",
 
@@ -42,7 +43,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- "sumneko_lua",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 3000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
@@ -80,5 +81,7 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+  -- Set python environment to use for neovim internal stuff
+  vim.g.python3_host_prog = '/home/ubuntu/miniconda3/bin/python3'
   end,
 }
