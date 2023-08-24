5069404c1a595401e24c6f601e080c8fee1cbedd
Ubuntu <ubuntu@instance-4.us-central1-a.c.crack-walker-265406.internal>
2023-08-24 12:15:45 +0000

    add global config for flake8 with null-ls

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,7 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,7 +11,9 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
-    }
+      null_ls.builtins.diagnostics.flake8.with({
+            extra_args = { "--config", "/home/ubuntu/ai_test/ai/.flake8" },
+    })}
     return config -- return final config table
   end,
 }
