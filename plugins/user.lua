9cbf3bf2d18543a543508a1e4f6e1880457bc234
Ubuntu <ubuntu@instance-4-mumbai.asia-south1-b.c.crack-walker-265406.internal>
2023-09-25 12:33:35 +0000

    add telescope media files i.e. a plugin to view images in nvim through telescope

--- plugins/user.lua
+++ plugins/user.lua
@@ -15,7 +15,25 @@ return {
   {
     "ojroques/nvim-osc52",
   },
-
+-- for project root directory setup and management
+-- {
+--   "ahmedkhalf/project.nvim",
+--    lazy=false,
+--
+--   config = function()
+--     require("project_nvim").setup {
+--         --require('telescope').load_extension('projects'),
+--         --require'telescope'.extensions.projects.projects{},
+--       -- your configuration comes here
+--       -- or leave it empty to use the default settings
+--       -- refer to the configuration section below
+--     }
+--       -- use "Telescope projects" commands to se recent projects
+--   end
+-- },
+-- setting up telescope media-files extension
+  {"nvim-lua/popup.nvim", lazy=false},
+  {"nvim-telescope/telescope-media-files.nvim", lazy=false},
   -- use vifm as file manager, lazy=false to make
   -- plugin load at start
   { "vifm/vifm.vim", lazy = false },
