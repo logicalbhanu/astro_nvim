ccb129d33e5f00c5b2523877dd79bfdbc64166a2
Ubuntu <ubuntu@instance-4-mumbai.asia-south1-b.c.crack-walker-265406.internal>
2023-09-25 12:32:41 +0000

    enable lua and markdown formatter via null-ls

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -9,8 +9,8 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
       null_ls.builtins.diagnostics.flake8.with({
             extra_args = { "--config", "/home/ubuntu/ai_test/ai/.flake8" },
     })}
