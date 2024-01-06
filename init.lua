72d8894226dcb565647204c8cc1f99eb2ba7cbc3
n3011 <mrinal.haloi11@gmail.com>
2024-01-06 15:17:21 +0000

    fix pylsp to have flake8 working as its plugin
    this will remove the need for null-ls to use flake8 and allow use pylsp plugin for flake8.

--- init.lua
+++ init.lua
@@ -1,3 +1,23 @@
+-- local venv_path = os.getenv('VIRTUAL_ENV')
+-- local py_path = nil
+-- -- decide which python executable to use for mypy
+-- if venv_path ~= nil then
+--   py_path = venv_path .. "/bin/python3"
+-- else
+--   py_path = vim.g.python3_host_prog
+-- end
+--
+local get_python_path = function()
+  local command = "which python"
+  local handle = io.popen(command)
+  local result = handle:read "*a"
+  handle:close()
+  return result
+end
+
+local py_path = get_python_path()
+-- print(py_path)
+
 return {
   -- Configure AstroNvim updates
   updater = {
@@ -18,7 +38,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -47,9 +67,70 @@ return {
       --   return true
       -- end
     },
-    -- enable servers that you already have installed without mason
+    -- enable servers that you already have installed without mason or if installed
+    -- with mason then using below config for that server
     servers = {
       -- "pyright"
+      "pylsp",
+    },
+    config = {
+      -- Notes about pylsp:
+      -- to install pylsp with mason use masoninstall command,
+      -- this will not install all the plugins for the pylsp for
+      -- that another command is given by mason wich is ":PylspInstall"
+      -- use it like this ":PylspInstall pyls-flake8 pylsp-mypy pyls-isort"
+      -- in vim, basically to installed all the plugin packages that we
+      -- usually install with 'pip', use PylspInstall command but in vim
+      -- command line as it will install these plugins then in the virtual
+      -- environment mason use to install the pylsp
+      --
+      -- specifically for the below config the needed plugins can be installed
+      -- with one single command
+      -- ":PylspInstall pyls-flake8 pylsp-mypy pyls-isort python-lsp-black python-lsp-ruff"
+      pylsp = {
+        plugins = {
+          -- formatter options
+          black = { enabled = true },
+          autopep8 = { enabled = false },
+          yapf = { enabled = false },
+          -- linter options
+          ruff = {
+            enabled = true,
+            --select = { "ALL" },
+            -- this is to select the rules that we want to include
+            -- in the diagnostics for the ruff lsp, similarly for format.
+            format = { "ALL" },
+            extendSelect = { "I" },
+          },
+          pylint = { enabled = false, executable = "pylint" },
+          pyflakes = { enabled = false },
+          pycodestyle = { enabled = false },
+          mccabe = { enabled = false },
+          flake8 = { enabled = true },
+          -- flake8 = { enabled = true,  maxLineLength=120, ignore={"E501","F401"} },
+          -- these lines are working in nvchad but not here thus using 'setup.cfg'
+          -- which is a config file for falke 8 at project root
+          -- first neovim will follow setup.cfg and if not found then it follows
+          -- these inline settings.
+          -- type checker
+          pylsp_mypy = {
+            enabled = true,
+            -- this will make mypy to use this python while looking for type stubs
+            -- which we have set to activated virtual environment
+            overrides = { "--python-executable", py_path, true },
+            report_progress = true,
+            live_mode = false,
+          },
+          -- auto-completion options
+          jedi_completion = { fuzzy = true },
+          -- import sorting
+          pyls_isort = { enabled = true },
+        },
+        configurationSources = { "flake8" },
+      },
+      -- pyright = {
+      --     reportMissingImports = false
+      --   },
     },
   },
 
@@ -81,12 +162,14 @@ return {
     --   },
     -- }
 
+    -- handling abnormal colors in tmux and neovim
+    vim.o.termguicolors = true
+
     -- setting color for matching parenthesis highlighting, earlier it
     -- was not visible
     vim.api.nvim_command "highlight MatchParen ctermbg=darkcyan guibg=darkcyan"
 
-    -- Set python environment to use for neovim internal stuff, not working 
-    -- due to some path stuff, thus change the python used, earlier using base env.
-    vim.g.python3_host_prog = "/home/ubuntu/miniconda3/envs/label_studio/bin/python"
+    -- Set python environment to use for neovim internal stuff,
+    vim.g.python3_host_prog = "/home/ubuntu/miniconda3/bin/python3"
   end,
 }
