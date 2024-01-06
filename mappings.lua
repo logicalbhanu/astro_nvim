4b7221e1154ef236e3a4617c245fa3407d746e83
n3011 <mrinal.haloi11@gmail.com>
2024-01-06 15:18:12 +0000

    reformat

--- mappings.lua
+++ mappings.lua
@@ -4,60 +4,66 @@
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick to close",
-    },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+	-- first key is the mode
+	n = {
+		-- second key is the lefthand side of the map
+		-- mappings seen under group name "Buffer"
+		["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+		["<leader>bD"] = {
+			function()
+				require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+					require("astronvim.utils.buffer").close(bufnr)
+				end)
+			end,
+			desc = "Pick to close",
+		},
+		-- tables with the `name` key will be registered with which-key if it's installed
+		-- this is useful for naming menus
+		["<leader>b"] = { name = "Buffers" },
+		-- quick save
+		-- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
 
-    -- better buffer navigation with Shift+l and h.
-    -- setting a mapping to false will disable it
-    ["]b"] = false,
-    ["[b"] = false,
-    ["<tab>"] = {
-      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-      desc = "Next buffer",
-    },
-    ["<S-tab>"] = {
-      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-      desc = "Previous buffer",
-    },
-    ["<S-t>"] = { "<C-6><cr>", desc = "last used tab" },
-  },
-  v = {
-    -- copy selection to client clipboard
-    ["<leader>c"] = {
-      require("osc52").copy_visual,
-      desc = "Copy to client",
-    },
-  },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-    -- shift-escape to exit terminal
-    ["<leader><ESC><ESC>"] = { "<C-\\><C-n>:q<cr>", desc = "Terminal quit" },
-    -- Enter normal mode in terminal
-    ["<leader><ESC>"] = { "<C-\\><C-n>", desc = "Normal mode" },
+		-- better buffer navigation with Shift+l and h.
+		-- setting a mapping to false will disable it
+		["]b"] = false,
+		["[b"] = false,
+		["<tab>"] = {
+			function()
+				require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+			end,
+			desc = "Next buffer",
+		},
+		["<S-tab>"] = {
+			function()
+				require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+			end,
+			desc = "Previous buffer",
+		},
+		["<S-t>"] = { "<C-6><cr>", desc = "last used tab" },
+	},
+	v = {
+		-- copy selection to client clipboard
+		["<leader>c"] = {
+			require("osc52").copy_visual,
+			desc = "Copy to client",
+		},
+	},
+	t = {
+		-- setting a mapping to false will disable it
+		-- ["<esc>"] = false,
+		-- shift-escape to exit terminal
+		["<leader><ESC><ESC>"] = { "<C-\\><C-n>:q<cr>", desc = "Terminal quit" },
+		--alternate key-binding to close terminal as esc clash with fish shell escape in vi-mode
+		["<leader>tf"] = { "<C-\\><C-n>:q<cr>", desc = "Terminal quit" },
+		-- Enter normal mode in terminal
+		["<leader><ESC>"] = { "<C-\\><C-n>", desc = "Normal mode" },
 
-    -- Window movement from terminal and within the terminal(toggleterm is also a temrinal)
-    ["<C-w>h"] = { "<C-\\><C-n><C-w>h", desc = "window left" },
-    ["<C-w>j"] = { "<C-\\><C-n><C-w>j", desc = "window below" },
-    ["<C-w>k"] = { "<C-\\><C-n><C-w>k", desc = "window above" },
-    ["<C-w>l"] = { "<C-\\><C-n><C-w>l", desc = "window right" },
-    -- to enable cycling between windows smooth in terminal(toggleterm)
-    ["<C-w><C-w>"] = { "<C-\\><C-n><C-w><C-w>", desc = "cycle window" },
-  },
+		-- Window movement from terminal and within the terminal(toggleterm is also a temrinal)
+		["<C-w>h"] = { "<C-\\><C-n><C-w>h", desc = "window left" },
+		["<C-w>j"] = { "<C-\\><C-n><C-w>j", desc = "window below" },
+		["<C-w>k"] = { "<C-\\><C-n><C-w>k", desc = "window above" },
+		["<C-w>l"] = { "<C-\\><C-n><C-w>l", desc = "window right" },
+		-- to enable cycling between windows smooth in terminal(toggleterm)
+		["<C-w><C-w>"] = { "<C-\\><C-n><C-w><C-w>", desc = "cycle window" },
+	},
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,40 +1,66 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  -- plugin to copy content to client in visual mode
-  -- if this system is uses as server and this neovim
-  -- instance is in use.
-  {
-    "ojroques/nvim-osc52",
-  },
--- for project root directory setup and management
--- {
---   "ahmedkhalf/project.nvim",
---    lazy=false,
---
---   config = function()
---     require("project_nvim").setup {
---         --require('telescope').load_extension('projects'),
---         --require'telescope'.extensions.projects.projects{},
---       -- your configuration comes here
---       -- or leave it empty to use the default settings
---       -- refer to the configuration section below
---     }
---       -- use "Telescope projects" commands to se recent projects
---   end
--- },
--- setting up telescope media-files extension
-  {"nvim-lua/popup.nvim", lazy=false},
-  {"nvim-telescope/telescope-media-files.nvim", lazy=false},
-  -- use vifm as file manager, lazy=false to make
-  -- plugin load at start
-  { "vifm/vifm.vim", lazy = false },
+	-- You can also add new plugins here as well:
+	-- Add plugins, the lazy syntax
+	-- "andweeb/presence.nvim",
+	-- {
+	--   "ray-x/lsp_signature.nvim",
+	--   event = "BufRead",
+	--   config = function()
+	--     require("lsp_signature").setup()
+	--   end,
+	-- },
+	-- plugin to copy content to client in visual mode
+	-- if this system is uses as server and this neovim
+	-- instance is in use.
+	{
+		"ojroques/nvim-osc52",
+	},
+	-- for project root directory setup and management
+	-- {
+	--   "ahmedkhalf/project.nvim",
+	--    lazy=false,
+	--
+	--   config = function()
+	--     require("project_nvim").setup {
+	--         --require('telescope').load_extension('projects'),
+	--         --require'telescope'.extensions.projects.projects{},
+	--       -- your configuration comes here
+	--       -- or leave it empty to use the default settings
+	--       -- refer to the configuration section below
+	--     }
+	--       -- use "Telescope projects" commands to se recent projects
+	--   end
+	-- },
+	-- setting up telescope media-files extension
+	{ "nvim-lua/popup.nvim", lazy = false },
+	{ "nvim-telescope/telescope-media-files.nvim", lazy = false },
+	-- use vifm as file manager, lazy=false to make
+	-- plugin load at start
+	{ "vifm/vifm.vim", lazy = false },
+
+	-- add plugin for catppuccin theme.
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			-- configuration options...
+			flavour = "mocha",
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+			},
+		},
+	},
+
+	-- to provide change surround syntax with repetition
+	{ "tpope/vim-surround", lazy = false, dependencies = { "tpope/vim-repeat" } },
+	{ "tpope/vim-repeat" },
 }
