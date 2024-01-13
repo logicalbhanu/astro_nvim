227e5b03cbb5a35669ccd621ffc70912dbc22002
Ubuntu <ubuntu@instance-4-mumbai.asia-south1-b.c.crack-walker-265406.internal>
2024-01-13 09:34:47 +0000

    remove pylsp from ensure installed.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		-- overrides `require("mason-lspconfig").setup(...)`
 		opts = {
-			ensure_installed = { "lua_ls", "pyright", "jsonls", "pylsp" },
+			ensure_installed = { "lua_ls", "pyright", "jsonls"} --, "pylsp" },
 		},
 	},
 	-- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
