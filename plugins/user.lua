8fd95f4cf4701c7277294e9bdd79dd724741a06c
Ubuntu <ubuntu@instance-4-mumbai.asia-south1-b.c.crack-walker-265406.internal>
2024-01-13 08:44:38 +0000

    setup codeium.vim plugin to have copilot like ghost complition

--- plugins/user.lua
+++ plugins/user.lua
@@ -93,4 +93,32 @@ return {
 			})
 		end,
 	},
+
+	-- configuring codeium
+	-- https://github.com/Exafunction/codeium
+	-- the below is specifically for neovim but i am unable to
+	-- make it work with neovim
+	-- {
+	-- 	"Exafunction/codeium.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"hrsh7th/nvim-cmp",
+	-- 	},
+	-- 	config = function()
+	-- 		require("codeium").setup({})
+	-- 	end,
+	-- },
+	{
+		"Exafunction/codeium.vim",
+		event = "BufEnter",
+		config = function()
+			vim.g.codeium_no_map_tab = 1
+			vim.keymap.set("i", "<C-g>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true })
+			-- vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+			-- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+			-- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+		end,
+	},
 }
