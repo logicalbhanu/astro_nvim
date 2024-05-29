2eac2601c63683d81f47341ab4c5072070c3adc9
logicalbhanu <pratapbhanumishra@gmail.com>
2024-05-29 19:03:04 +0530

    add pylance support
    add the support for pylance, but still not getting any suggestions in
    buffer, though lsp is seen as installed in the status bar

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,18 +5,47 @@
 ---@type LazySpec
 return {
   -- use mason-lspconfig to configure LSP installations
+  -- {
+  --   "williamboman/mason-lspconfig.nvim",
+  --   -- overrides `require("mason-lspconfig").setup(...)`
+  --   opts = function(_, opts)
+  --     -- add more things to the ensure_installed table protecting against community packs modifying it
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  --       "lua_ls",
+  --       "pylsp",
+  --       -- add more arguments for adding more language servers
+  --     })
+  --   end,
+  -- },
+
+  -- mason config to handle pylance too
   {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
-        "pylsp",
-        -- add more arguments for adding more language servers
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "lua:custom-registry", -- custom user registry
+        "github:mason-org/mason-registry", -- make sure to add the default registry
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+
+      local cmd = vim.api.nvim_create_user_command
+      cmd("MasonUpdate", function(options) require("astrocore.mason").update(options.fargs) end, {
+        nargs = "*",
+        desc = "Update Mason Package",
+        complete = function(arg_lead)
+          local _ = require "mason-core.functional"
+          return _.sort_by(
+            _.identity,
+            _.filter(_.starts_with(arg_lead), require("mason-registry").get_installed_package_names())
+          )
+        end,
       })
+      cmd("MasonUpdateAll", function() require("astrocore.mason").update_all() end, { desc = "Update Mason Packages" })
     end,
   },
+
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
